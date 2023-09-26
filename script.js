0968b2d56ef994fff631e91a22f460c60d0ee268
Baldo Acerboni <baldo.acerboni@gmail.com>
2023-09-26 14:46:36 +0200

    Fix bugs and add functionalities
    -add max digits value(10) and what to do in case user tries to input further numbers
    
    -add max decimal digits(3) and what to do in case user tries to input further numbers
    
    -output also limited to max 3 decimal digits to avoid floating point number conundrum
    
    -fix bug where if user clicked precisely on children of 'squared' or 'powOf' the output
    would use the value of nested element end not of button element
    
    -improve 'ERROR' handling
    
    -change of color of delete button

--- script.js
+++ script.js
@@ -1,49 +1,75 @@
 const currentOutput = document.getElementById('current');
 const previousOutput = document.getElementById('previous');
-const buttons = document.getElementsByClassName('button');
 const numberBtn = document.getElementsByClassName('num');
 const operatorBtn = document.getElementsByClassName('op');
 const equalBtn = document.getElementById('equal');
 const deleteBtn = document.getElementById('delete');
-
+//variable used to store the values to be used in the operation
 const calculator = {
     num: '',
-    elementsArray: [],
-    
+    elementsArray: []
 }
 
 //FUNCTIONS
-function displayOutput (e) {
-    if(e.target.id) {
-        currentOutput.textContent += e.target.id;
+function writeNumber(e) {
+    let textContentTemp = '';
+    //I want numbers not IP addresses
+    if(e.target.textContent === '.' && calculator.num.indexOf('.') !== -1) {
+        return;
+    //max 3 decimal digits. In that case substitute last digit with user input
+    } else if(calculator.num.indexOf('.') !== -1 && calculator.num.indexOf('.') === calculator.num.length - 4) {
+        calculator.num = calculator.num.split('');
+        calculator.num.pop();//modifies actual variable and does not create a copy like other methods used
+        calculator.num = calculator.num.join('') + e.target.textContent;
+        textContentTemp = currentOutput.textContent;
+        textContentTemp = textContentTemp.split('');
+        textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
+        textContentTemp = textContentTemp.join('');
+        currentOutput.textContent = textContentTemp + e.target.textContent;
+        textContentTemp = '';
+    //max ten digits number, not counting decimals. In that case substitute last digit with user input
+    } else if(e.target.textContent !== '.' && calculator.num.indexOf('.') === -1 && calculator.num.length === 10) {
+        calculator.num = calculator.num.split('');
+        calculator.num.pop();//modifies actual variable and does not create a copy like other methods used
+        calculator.num = calculator.num.join('') + e.target.textContent;
+        textContentTemp = currentOutput.textContent;
+        textContentTemp = textContentTemp.split('');
+        textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
+        textContentTemp = textContentTemp.join('');
+        currentOutput.textContent = textContentTemp + e.target.textContent;
+        textContentTemp = '';
     } else {
+        calculator.num += e.target.textContent;
+        currentOutput.textContent += e.target.textContent;
+    }
+}
+
+function writeOperator(e) {
+    if(calculator.num){
+        calculator.elementsArray.push(+calculator.num);
+        calculator.num = '';
+    }
+    if(e.target.id === 'squared') {
+        calculator.elementsArray.push('**', 2)
+        currentOutput.textContent += e.target.id;
+    } else if (e.target.id) {
+        calculator.elementsArray.push('**')
+        currentOutput.textContent += e.target.id;
+    } else if(e.target.parentNode.id === 'squared') {
+        calculator.elementsArray.push('**', 2)
+        currentOutput.textContent += e.target.parentNode.id;
+    } else if(e.target.parentNode.id === 'powOf') {
+        calculator.elementsArray.push('**')
+        currentOutput.textContent += e.target.parentNode.id;
+    } else {
+        calculator.elementsArray.push(e.target.textContent)
         currentOutput.textContent += e.target.textContent;
     }
     
 }
 
-function displayDelete() {
-    currentOutput.textContent = '';
-}
-
-function writeNumber(e) {
-    calculator.num += e.target.textContent;
-}
-
-function writeOperator(e) {
-    calculator.elementsArray.push(+calculator.num);
-    calculator.num = '';
-    if(e.target.id === 'squared') {
-        calculator.elementsArray.push('**', 2)
-        console.log(calculator.elementsArray)
-    } else if (e.target.id) {
-        calculator.elementsArray.push('**')
-    } else {
-        calculator.elementsArray.push(e.target.textContent)
-    }
-}
-
 function deleteObj() {
+    currentOutput.textContent = '';
     calculator.num = '';
     calculator.elementsArray = [];
 }
@@ -53,69 +79,57 @@ function solve() {
         calculator.elementsArray.push(+calculator.num)
     }
     const solved = solveExpression(calculator.elementsArray);
-    console.log(solved)
     previousOutput.textContent = currentOutput.textContent;
     currentOutput.textContent = solved;
     calculator.num = solved.toString();
-
+    calculator.elementsArray = [];
 }
 
-//NON FUNZIONA DOPO UGUALE
+//function that solve the expression, used recursion to define priority
 function solveExpression(arr) {
     let num = 0;
     if(arr.length === 1) {
+        //case 1++++4 or similar
+        if(isNaN(arr[0])) {
+            arr[0] = 'ERROR';
+        //cheating floating point bs
+        } else if(arr[0] % 1) {
+            arr[0] = arr[0].toFixed(3);
+        }
         return;
     } else if(arr.length === 2) {
+        //due to the if statement at the beginning of solve() that 'uses' the last number input of the user only
+        //if an operator has been inserted right before it, i could not find a way to get an error
+        //but it is probably just my smooth brain that can not think of it so better leave it there
+        console.log(arr)
         arr[0] = 'ERROR'
         return;
     } else if(arr.indexOf('**') !== -1) {
         num = arr[arr.indexOf('**') - 1] ** arr[arr.indexOf('**') + 1];
         arr.splice(arr.indexOf('**')-1, 3, num)
-        console.log(num)
-        console.log(arr)
-        num = 0;
         solveExpression(arr);
+        console.log(arr)
     } else if(arr.indexOf('*') !== -1) {
-        console.log(arr.indexOf('*'))
         num = arr[arr.indexOf('*') - 1] * arr[arr.indexOf('*') + 1];
         arr.splice(arr.indexOf('*')-1, 3, num)
-        console.log(num)
-        console.log(arr)
-        num = 0;
         solveExpression(arr);
     } else if(arr.indexOf('/') !== -1) {
         num = arr[arr.indexOf('/') - 1] / arr[arr.indexOf('/') + 1];
         arr.splice(arr.indexOf('/')-1, 3, num)
-        console.log(num)
-        console.log(arr)
-        num = 0;
         solveExpression(arr);
     } else if(arr.indexOf('-') !== -1) {
         num = arr[arr.indexOf('-') - 1] - arr[arr.indexOf('-') + 1];
         arr.splice(arr.indexOf('-')-1, 3, num)
-        console.log(num)
-        console.log(arr)
-        num = 0;
         solveExpression(arr);
     } else if(arr.indexOf('+') !== -1) {
         num = arr[arr.indexOf('+') - 1] + arr[arr.indexOf('+') + 1];
         arr.splice(arr.indexOf('+')-1, 3, num)
-        console.log(num)
-        console.log(arr)
-        num = 0;
         solveExpression(arr);
     }
     return arr[0];
 }
 
 //EVENT LISTENERS
-Array.from(buttons).forEach(button => {
-    button.addEventListener('click', displayOutput)
-})
-
-deleteBtn.addEventListener('click', displayDelete)
-
-
 Array.from(numberBtn).forEach(btn => {
 btn.addEventListener('click', writeNumber)
 })
