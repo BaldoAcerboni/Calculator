473187ec08b412ad762eb74d4238eba9643cc9be
Baldo Acerboni <baldo.acerboni@gmail.com>
2023-09-27 15:49:05 +0200

    Add keyboard event listener

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ const calculator = {
     num: '',
     elementsArray: []
 }
+const keyboardEvents = {
+    numbers: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.', 'Backspace'],
+    operators: ['+', '-', '*', '/', '^']
+}
 
 //FUNCTIONS
 function writeNumber(e) {
@@ -21,68 +25,54 @@ function writeNumber(e) {
         previousOutput.textContent = '';
     }
     if(typeof calculator.elementsArray[calculator.elementsArray.length - 1] === 'number') {
-        para.textContent = 'Please choose an operation you want to apply to the previously squared number';
-    } else if(e.target.textContent === '.' && !calculator.num) {
+        para.textContent = 'Please choose an operation you want to apply to the previous number';
+    } else if((e.target.textContent === '.' || e.key === '.') && !calculator.num) {
         calculator.num = '0.';
         currentOutput.textContent = '0.';
+    
+    } else if(e.target.classList.contains('backspace') || e.target.parentNode.classList.contains('backspace') || e.key === 'Backspace') {
+        calculator.num = calculator.num.split('');
+        calculator.num.pop();
+        calculator.num = calculator.num.join('');
+        textContentTemp = currentOutput.textContent;
+        textContentTemp = textContentTemp.split('');
+        textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
+        textContentTemp = textContentTemp.join('');
+        currentOutput.textContent = textContentTemp;
     //I want numbers not IP addresses
-    } else if(e.target.textContent === '.' && calculator.num.indexOf('.') !== -1) {
+    } else if((e.target.textContent === '.' || e.key === '.') && calculator.num.indexOf('.') !== -1) {
+        para.textContent = 'Please input valid numbers'
         return;
     //max 3 decimal digits. In that case substitute last digit with user input
     } else if(calculator.num.indexOf('.') !== -1 && calculator.num.indexOf('.') < calculator.num.length - 3) {
-        if(e.target.textContent !== '00'){//I really regret the '00' button right now
-            calculator.num = calculator.num.split('');
-            calculator.num.pop();//modifies actual variable and does not create a copy like other methods used
-            calculator.num = calculator.num.join('') + e.target.textContent;
-            textContentTemp = currentOutput.textContent;
-            textContentTemp = textContentTemp.split('');
-            textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
-            textContentTemp = textContentTemp.join('');
-            currentOutput.textContent = textContentTemp + e.target.textContent;
-            textContentTemp = '';
-        } else {
-            calculator.num = calculator.num.split('');
-            calculator.num.pop();
-            calculator.num.pop();//modifies actual variable and does not create a copy like other methods used
-            calculator.num = calculator.num.join('') + e.target.textContent;
-            textContentTemp = currentOutput.textContent;
-            textContentTemp = textContentTemp.split('');
-            textContentTemp.pop();
-            textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
-            textContentTemp = textContentTemp.join('');
-            currentOutput.textContent = textContentTemp + e.target.textContent;
-            textContentTemp = '';
-        }
+        calculator.num = calculator.num.split('');
+        calculator.num.pop();//modifies actual variable and does not create a copy like other methods used
+        calculator.num = calculator.num.join('') + (e.key || e.target.textContent);
+        textContentTemp = currentOutput.textContent;
+        textContentTemp = textContentTemp.split('');
+        textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
+        textContentTemp = textContentTemp.join('');
+        currentOutput.textContent = textContentTemp + (e.key || e.target.textContent);
+        textContentTemp = '';
         para.textContent = 'Max number of decimal digits reached. What previously was the last digit, has been replaced with the last input';
     //max ten digits number, not counting decimals. In that case substitute last digit with user input
-    } else if(e.target.textContent !== '.' && calculator.num.indexOf('.') === -1 && calculator.num.length > 9) {
-        if(e.target.textContent !== '00') {//REALLY regretting my life choices right about now
-            calculator.num = calculator.num.split('');
-            calculator.num.pop();//modifies actual variable and does not create a copy like other methods used
-            calculator.num = calculator.num.join('') + e.target.textContent;
-            textContentTemp = currentOutput.textContent;
-            textContentTemp = textContentTemp.split('');
-            textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
-            textContentTemp = textContentTemp.join('');
-            currentOutput.textContent = textContentTemp + e.target.textContent;
-            textContentTemp = '';
-        } else {//forget it, I regret nothing, long live ctrl+c ctrl+v!!!
-            calculator.num = calculator.num.split('');
-            calculator.num.pop();
-            calculator.num.pop();//modifies actual variable and does not create a copy like other methods used
-            calculator.num = calculator.num.join('') + e.target.textContent;
-            textContentTemp = currentOutput.textContent;
-            textContentTemp = textContentTemp.split('');
-            textContentTemp.pop();
-            textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
-            textContentTemp = textContentTemp.join('');
-            currentOutput.textContent = textContentTemp + e.target.textContent;
-            textContentTemp = '';
-        }
+    } else if((e.key === '.' || e.target.textContent === '.') && calculator.num.indexOf('.') === -1) {
+        calculator.num += (e.key || e.target.textContent);
+        currentOutput.textContent += (e.key || e.target.textContent);
+    } else if(calculator.num.indexOf('.') === -1 && calculator.num.length > 9) {
+        calculator.num = calculator.num.split('');
+        calculator.num.pop();//modifies actual variable and does not create a copy like other methods used
+        calculator.num = calculator.num.join('') + (e.key || e.target.textContent);
+        textContentTemp = currentOutput.textContent;
+        textContentTemp = textContentTemp.split('');
+        textContentTemp.pop();//modifies actual variable and does not create a copy like other methods used
+        textContentTemp = textContentTemp.join('');
+        currentOutput.textContent = textContentTemp + (e.key || e.target.textContent);
+        textContentTemp = '';
         para.textContent = 'Max number of digits reached. What previously was the last digit, has been replaced with the last input';
     } else {
-        calculator.num += e.target.textContent;
-        currentOutput.textContent += e.target.textContent;
+        calculator.num += (e.key || e.target.textContent);
+        currentOutput.textContent += (e.key || e.target.textContent);
     }
 }
 
@@ -97,7 +87,7 @@ function writeOperator(e) {
             calculator.elementsArray.push('**', 2)
             currentOutput.textContent = '';
             previousOutput.textContent = calculator.elementsArray.join('')
-        } else if (e.target.id) {
+        } else if (e.key === '^' || e.target.id === 'powOf') {
             calculator.elementsArray.push('**')
             currentOutput.textContent = '';
             previousOutput.textContent = calculator.elementsArray.join('')
@@ -110,26 +100,26 @@ function writeOperator(e) {
             currentOutput.textContent = '';
             previousOutput.textContent = calculator.elementsArray.join('')
         } else {
-            calculator.elementsArray.push(e.target.textContent)
+            calculator.elementsArray.push(e.key || e.target.textContent)
             currentOutput.textContent = '';
             previousOutput.textContent = calculator.elementsArray.join('')
         }
     } else {
         if(e.target.id === 'squared') {
             para.textContent = 'please input the number you want to square';
-        } else if (e.target.id) {
+        } else if (e.key === '^' || e.target.id === 'powOf') {
             para.textContent = 'please input the number you want to elevate to the power of n';
         } else if(e.target.parentNode.id === 'squared') {
             para.textContent = 'please input the number you want to square';
         } else if(e.target.parentNode.id === 'powOf') {
             para.textContent = 'please input the number you want to elevate to the power of n';
-        } else if(e.target.textContent === '+'){
+        } else if(e.key === '+' || e.target.textContent === '+'){
             para.textContent = `please input the number you want sum`;
-        } else if(e.target.textContent === '-'){
+        } else if(e.key === '-' || e.target.textContent === '-'){
             para.textContent = `please input the number you want subtract`;
-        } else if(e.target.textContent === '/'){
+        } else if(e.key === '/' || e.target.textContent === '/'){
             para.textContent = `please input the number you want divide`;
-        } else if(e.target.textContent === '*'){
+        } else if(e.key === '*' || e.target.textContent === '*'){
             para.textContent = `please input the number you want multiply`;
         }
     }
@@ -146,7 +136,8 @@ function deleteObj() {
         calculator.elementsArray = [];
     }
 }
-//function that checks if what user is trying to solve makes sense, if so sends input to solveExpression()
+
+//function that checks if what user is trying to solve makes sense, if so it gets result from solveExpression()
 function solve() {
     para.textContent = '';
     if(calculator.elementsArray.length <= 1) {
@@ -203,6 +194,27 @@ function solveExpression(arr) {
     return arr[0];
 }
 
+function getKey(e) {
+    if(keyboardEvents.numbers.indexOf(e.key) !== -1) {
+        writeNumber(e);
+    } else if(keyboardEvents.operators.indexOf(e.key) !== -1) {
+        writeOperator(e);
+    } else if(e.key === 'Delete') {
+        deleteObj();
+    } else if(e.key === '=') {
+        solve();
+    } else if(e.key === 'Shift') {
+        return;
+    } else {
+        para.textContent = `Keyboard cheat-sheet:
+        besides the obvious, 
+        "^" to elevate to the power of n,
+        "Backspace" to delete a single digit,
+        "Delete" to delete last input full number, press again to delete whole expression`;
+        return;
+    }
+}
+
 //EVENT LISTENERS
 Array.from(numberBtn).forEach(btn => {
 btn.addEventListener('click', writeNumber)
@@ -215,3 +227,5 @@ Array.from(operatorBtn).forEach(btn => {
 deleteBtn.addEventListener('click', deleteObj)
 
 equalBtn.addEventListener('click', solve)
+
+window.addEventListener('keyup', getKey)
