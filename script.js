5a604cd83a3dc641b1652d54a83de847aa5a0f4d
Gnagno <baldo.acerboni@gmail.com>
2025-06-14 07:33:29 +0200

    FIll out oart of javascript

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const calcHistory = document.querySelector(".past-calc");
 const currentCalc = document.querySelector(".current-calc");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const numArr = [];
+const operationArr = [];
 
+//basic operation functions
 function add(n1, n2) {
   return n1 + n2;
 }
@@ -25,8 +30,9 @@ function squareRoot(n) {
   return Math.sqrt(n);
 }
 
+//function that decides which operation to fire
 function decideOperation(arr) {
-  if (arr[0] === "&radic;" && !isNaN(arr[1]) && arr.length === 2) {
+  if (!isNaN(arr[1]) && arr.length === 2) {
     return squareRoot(arr[1]);
   } else if (arr.length === 3) {
     if (arr[1] === "+") {
@@ -42,5 +48,39 @@ function decideOperation(arr) {
     }
   }
 }
-const arr = ["&radic;", 4];
-console.log(decideOperation(arr));
+
+//function that creates array containing numbers + operators and decide what to do with them
+function Calculator(input) {
+  let result;
+  if (!isNaN(+input) && operationArr.length === 0) {
+    operationArr.push(input);
+  } else if (
+    operationArr.length === 1 &&
+    input.classList.contains("operator")
+  ) {
+    operationArr.push(input);
+  } else if (
+    operationArr.length === 2 &&
+    operationArr[1] === "*" &&
+    input === "*"
+  ) {
+    operationArr[1] = "**";
+  } else if (operationArr.length === 2 && !isNaN(+input)) {
+    operationArr.push(input);
+  } else if (
+    operationArr.length === 3 &&
+    input.classList.contains("operator")
+  ) {
+    result = decideOperation(operationArr);
+    operationArr.length = 0;
+    operationArr.push(result);
+  }
+}
+
+function numberSetter(n) {
+  if (!isNaN(n)) {
+    numArr.push(n);
+  } else if (n === "." && numArr.indexOf(n) === -1) {
+    numArr.push(n);
+  }
+}
