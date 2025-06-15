0b4d9febc760c5c3627a462ae4ffb5d75259267a
Gnagno <baldo.acerboni@gmail.com>
2025-06-15 11:53:39 +0200

    Do calculator with consttructor function and see how that goes

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const currentScreen = document.querySelector(".current-calc");
+const pastScreen = document.querySelector(".past-calc");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const backspace = document.querySelector(".backspace");
+const deleteBtn = document.querySelector(".cancel");
+const equal = document.querySelector(".equal");
 
+function Calculator() {
+  (this.currentNum = ""),
+    (this.operationArr = []),
+    (this.add = function (n1, n2) {
+      return n1 + n2;
+    }),
+    (this.subtract = function (n1, n2) {
+      return n1 - n2;
+    }),
+    (this.multiply = function (n1, n2) {
+      return n1 * n2;
+    }),
+    (this.divide = function (n1, n2) {
+      return n1 / n2;
+    }),
+    (this.powerOf = function (n1, n2) {
+      return n1 ** n2;
+    });
+}
+
+let calc = new Calculator();
