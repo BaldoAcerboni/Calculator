22b54554e166f3e618ddb8fff1fd25d56b25463c
Gnagno <baldo.acerboni@gmail.com>
2025-06-16 09:54:54 +0200

    Seem like the right path

--- script.js
+++ script.js
@@ -6,82 +6,118 @@ const backspace = document.querySelector(".backspace");
 const deleteBtn = document.querySelector(".cancel");
 const equal = document.querySelector(".equal");
 
-function Calculator() {
-  (this.currentNum = ""),
-    (this.operationArr = []),
-    (this.setCurrentNumBtn = function (n) {
-      this.currentNum += n.target.textContent;
-    }),
-    (this.setOperationArrBtn = function (el) {
-      console.log(this.operationArr);
-      if (this.operationArr.length === 0) {
-        this.operationArr.push(this.currentNum);
-        this.currentNum = "";
-      }
-      this.operationArr.push(el.target.textContent);
-    }),
-    (this.setOperation = function (arr) {
-      if (arr.length === 3) {
-        if (arr[1] === "+") {
-          this.add(arr[0], arr[2]);
-        } else if (arr[1] === "-") {
-          this.subtract(arr[0], arr[2]);
-        } else if (arr[1] === "*") {
-          this.multiply(arr[0], arr[2]);
-        } else if (arr[1] === "/") {
-          this.divide(arr[0], arr[2]);
-        } else if (arr[1] === "^") {
-          this.powerOf(arr[0], arr[2]);
-        }
-      }
-    }),
-    (this.add = function (n1, n2) {
-      return n1 + n2;
-    }),
-    (this.subtract = function (n1, n2) {
-      return n1 - n2;
-    }),
-    (this.multiply = function (n1, n2) {
-      return n1 * n2;
-    }),
-    (this.divide = function (n1, n2) {
-      return n1 / n2;
-    }),
-    (this.powerOf = function (n1, n2) {
-      return n1 ** n2;
-    }),
-    (this.setCurrentScreenBtn = function (str) {
-      currentScreen.textContent = str.target.textContent;
-    }),
-    (this.setPastScreenBtn = function (str) {
-      pastScreen.textContent = currentScreen.textContent;
-      currentScreen.textContent = str.target.textContent;
-    }),
-    (this.backspace = function () {
-      if (this.currentNum) {
-        this.currentNum = "";
-      } else {
-        this.operationArr.pop();
-      }
-    }),
-    (this.insertOperator = function (op) {
-      if (this.operationArr.length === 1) {
-        this.operationArr.push(op);
-      } else if (this.operationArr.length === 3) {
-        this.operationArr;
-      }
-    });
+const calculator = {
+  currentNum: "",
+  operationArr: [],
+};
+
+const keybooardEvents = {
+  numbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 0],
+  operators: ["+", "-", "*", "/", "^"],
+  deleters: ["Backspace", "Delete"],
+  equal: "=",
+};
+function updateCurrentNumBtn(n) {
+  calculator.currentNum += n.target.textContent;
 }
 
-let calc = new Calculator();
-console.log(calc);
+function updateOperationArr(el) {
+  let result = 0;
+  if (calculator.currentNum && el.target.classList.contains("operator")) {
+    calculator.operationArr.push(parseFloat(calculator.currentNum));
+    calculator.currentNum = "";
+    calculator.operationArr.push(el.target.textContent);
+  }
+  console.log(calculator.operationArr.length);
+  if (
+    calculator.operationArr.length === 4 &&
+    el.target.classList.contains("operator")
+  ) {
+    console.log(calculator.operationArr.slice(0, -1));
+    result = setOperationArrBtn(calculator.operationArr.slice(0, -1));
+    console.log("result", result);
+    setPastScreenBtn(result);
+    setCurrentScreenBtn(el);
+    clearOpearationArr();
+    calculator.operationArr.push(result, el.target.textContent);
+  }
+
+  console.log("calculator.operationArr", calculator.operationArr);
+}
+
+function resetCurrentNum() {
+  calculator.currentNum = "";
+}
+
+function clearOpearationArr() {
+  calculator.operationArr.length = 0;
+}
+
+function setCurrentScreenBtn(str) {
+  currentScreen.textContent += str.target.textContent;
+}
+
+function setPastScreenBtn(str) {
+  pastScreen.textContent = currentScreen.textContent;
+  currentScreen.textContent = str;
+}
+function backspaceAction() {
+  if (calculator.currentNum) {
+    calculator.currentNum = "";
+  } else {
+    calculator.operationArr.pop();
+  }
+}
+
+function insertOperator(op) {
+  if (calculator.operationArr.length === 0 && calculator.currentNum) {
+    calculator.operationArr.push(calculator.currentNum);
+  }
+  if (calculator.operationArr.length === 1) {
+    calculator.operationArr.push(op);
+  } else if (calculator.operationArr.length === 3) {
+    calculator.operationArr;
+  }
+}
+
+function add(n1, n2) {
+  return n1 + n2;
+}
+function subtract(n1, n2) {
+  return n1 - n2;
+}
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+function divide(n1, n2) {
+  return n1 / n2;
+}
+function powerOf(n1, n2) {
+  return n1 ** n2;
+}
+
+function setOperationArrBtn(arr) {
+  if (arr.length === 3) {
+    if (arr[1] === "+") {
+      return add(arr[0], arr[2]);
+    } else if (arr[1] === "-") {
+      return subtract(arr[0], arr[2]);
+    } else if (arr[1] === "*") {
+      return multiply(arr[0], arr[2]);
+    } else if (arr[1] === "/") {
+      return divide(arr[0], arr[2]);
+    } else if (arr[1] === "^") {
+      return powerOf(arr[0], arr[2]);
+    }
+  }
+}
 
 Array.from(numbers).forEach((num) => {
-  num.addEventListener("click", calc.setCurrentNumBtn);
-
-  num.addEventListener("click", calc.setCurrentScreenBtn);
+  num.addEventListener("click", updateCurrentNumBtn);
+  num.addEventListener("click", setCurrentScreenBtn);
 });
 
 Array.from(operators).forEach((op) => {
-  op.addEventListener("click", calc.setOperationArrBtn);
+  op.addEventListener("click", setCurrentScreenBtn);
+  op.addEventListener("click", updateOperationArr);
 });
